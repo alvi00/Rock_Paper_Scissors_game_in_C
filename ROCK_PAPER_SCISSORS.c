aabbb1d4a0e7fef4810a6df0cb7fe5b1bac55a95
Ahmad Fahmid <112432485+alvi00@users.noreply.github.com>
2022-12-12 18:32:56 +0600

    Add files via upload

---
+++ ROCK_PAPER_SCISSORS.c
@@ -0,0 +1,156 @@
+#include<stdio.h>
+#include<stdlib.h>                      //for random numbers
+#include<time.h>                        //for time
+#include<stdbool.h>                     //to get bolian variable
+#include<windows.h>                      //to use cmd commands
+#include<conio.h>
+int score;
+
+
+#define ROCK 1
+#define PAPER 2
+#define SCISSORS 3
+void game();                            //full function for the game
+void load();
+int main()
+{
+    while(1)
+    {
+
+        printf("\n\n");
+        printf("\t\t                        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $    Select any of them -          $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $    1.  PLAY GAME                 $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $    2.  EXIT                      $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $                                  $\n");
+        printf("\t\t                        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+        int a;
+        scanf("%d",&a);
+        system("cls");
+
+        if(a==1)
+        {
+            load();
+            game();
+        }
+        else if(a==2)
+        {
+            return 0;
+        }
+        else
+        {
+        printf("\n -----------------------You Entered Wrong Number------------------------\n  ------------------------Enter Correct Number------------------------\n");
+        }
+    }
+
+
+    return 0;
+}
+void game()
+{
+    int t=3;
+    while(t--)
+    {
+    srand(time(NULL));                     //seeding to genarate random numbers with time
+    int  n;
+    int  player_throw = 0;
+    char ch_player_throw;                 //to store the player given numbers
+    int  ai_throw=0;                        //to store the ai given numbers
+    bool draw=false;                       //it will check the game is draw or not
+
+    do
+    {
+        printf("Select your throw\n");
+        printf("1) ROCK\n");
+        printf("2) PAPER\n");
+        printf("3) SCISSORS\n");
+        printf("SELECTION: ");
+        while(1)
+        {
+            ch_player_throw = getch();          //to store player numbers
+            player_throw = (int)(ch_player_throw - 48);
+
+            if(player_throw != 1 && player_throw != 2 && player_throw != 3)
+            {
+                Beep(450, 250);
+                continue;
+            }
+            else break;
+        }
+        printf("%c", ch_player_throw);
+        getch();
+
+
+        ai_throw = (rand() % 3) + 1;           //random number will give me 0 to infinity numbers so to get 1 2 3, It will mod that intezer and mod with 3 and it will give me 0,1,2 and to get 1,2,4 there is a plus 1
+
+        if(ai_throw==ROCK)                     // TO LET THE PLAYER KNOW WHAT AI GIVEN THEM
+        {
+            printf("\n AI GIVES ROCK.\n");
+        }
+        else if(ai_throw==PAPER)
+        {
+            printf("\n AI GIVES PAPER.\n");
+        }
+        else if(ai_throw== SCISSORS)
+        {
+            printf("\n AI GIVES SCISSORS.\n");
+        }
+
+        draw =false;
+        if (player_throw == ROCK && ai_throw == SCISSORS)
+        {
+             printf("\nROCK beats SCISSORS. YOU WIN.\n\n");
+             score++;
+        }
+
+        else if (player_throw == ROCK && ai_throw == PAPER)
+        {
+              printf("\nPAPER beats ROCK. YOU LOSE.\n\n");
+        }
+       else if (player_throw == SCISSORS && ai_throw == PAPER)
+        {
+              printf("\nSCISSORS beats PAPER. YOU WIN.\n\n");
+                score++;
+        }
+        else if (player_throw == SCISSORS && ai_throw == ROCK)
+        {
+              printf("\nROCK beats SCISSORS. YOU LOSE.\n\n");
+        }
+         else if (player_throw == PAPER && ai_throw == ROCK)
+         {
+              printf("\nPAPER beats ROCK. YOU WIN.\n\n");
+               score++;
+         }
+         else if (player_throw == PAPER && ai_throw == SCISSORS)
+         {
+         printf("\nSCISSORS beats PAPER. YOU LOSE.\n\n");
+         }
+    else
+    {
+      printf("\nDRAW! Play again.\n\n");            // if both player and AI throw the same, it's a draw
+      printf("YOUR SCORE IS :%d\n\n",score);
+      draw = true;
+    }
+
+    } while (draw);                                  // the game will run as long as it is draw
+    printf("YOUR SCORE IS :%d\n\n\n",score);
+    }
+    getch();
+    system("cls");
+}
+
+    void load()
+{
+    int r,q;
+    printf("\n\n\n\t\t\tloading...");
+    for(r=1;r<=20;r++){
+    for(q=0;q<=100000000;q++);   //to display the character slowly
+    printf("%c",177);}
+    system("cls");
+}
